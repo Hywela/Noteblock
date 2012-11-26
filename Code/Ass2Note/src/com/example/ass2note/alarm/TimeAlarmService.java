d17583683210fa43f062e8ee502bbc0fd02357df
SolveigS <SolveigHig@gmail.com>
2012-11-26 15:16:51 +0100

    Last

---
+++ Code/Ass2Note/src/com/example/ass2note/alarm/TimeAlarmService.java
@@ -0,0 +1,33 @@
+package com.example.ass2note.alarm;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+
+/*
+ * Her kan du kode alt som skal skje dersom alarmen gjekk av. Dette er ganske
+ * likt LocationAlarmService, så vi burde finne en måte å bruke samme kodane på.
+ * Men det viktigaste er at det fungerar først. 
+ */
+public class TimeAlarmService extends Service {
+    public TimeAlarmService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    @Override
+    public void onCreate() {
+    	super.onCreate();
+    	
+    	Context c = getApplicationContext();
+    	Toast.makeText(c, "Inside TimeAlarmService!", Toast.LENGTH_LONG).show();
+    	stopSelf();
+    }
+}
---
+++ Code/Ass2Note/src/com/example/ass2note/location/ConnectionService.java
@@ -0,0 +1,72 @@
+package com.example.ass2note.location;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ConnectionService extends IntentService {
+	public ConnectionService() {
+		super("ConnectionService");
+	}
+
+	public ConnectionService(String name) {
+		super(name);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Log.i("ConnectionService", "onHandleIntent");
+		String fromActivity = intent.getStringExtra("fromActivity");
+		if (fromActivity.contains("NoteEdit")) 
+			sendBroadcastTo("com.example.ass2note.notepad.NoteEdit.connectionReceiver");
+		else if(fromActivity.contains("LocationAlarmReceiver"))
+			sendBroadcastTo("com.example.ass2note.alarm.LocationAlarmService.LASReceiver");
+	}
+
+	private void sendBroadcastTo(String receiver){
+		Intent i = new Intent(receiver);
+		i.putExtra("gpsEnabled", isGPSEnabled());
+		i.putExtra("networkEnabled", isNetworkEnabled());
+		sendBroadcast(i);
+	}
+	
+	private boolean isGPSEnabled() {
+		// This verification should be done during onStart() because the system
+		// calls
+		// this method when the user returns to the activity, which ensures the
+		// desired
+		// location provider is enabled each time the activity resumes from the
+		// stopped state.
+		LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+		boolean gpsEnabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+		return gpsEnabled;
+	}
+
+	private boolean isNetworkEnabled() {
+		boolean networkConnection = false;
+		
+		ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		
+		// For 3G check
+		 boolean is3g = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting();
+		 
+		// For WiFi Check
+		boolean isWifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+				.isConnectedOrConnecting();
+
+		if (is3g || isWifi) networkConnection = true;
+		 
+		return networkConnection;
+	}
+
+}
